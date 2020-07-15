a5cf8593d8dd3df3713ee18b4d98f685b7161835
Dagbouj-Hatem <dagboujhatem@gmail.com>
2020-07-15 11:51:38 +0100

    delete unused ressource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,11 +52,7 @@ import { AuthInterceptor } from './common/interceptor';
 import { SuperAdmin } from './views/superadmin/superadmin.module';
 import { ResetComponent } from './common/reset/reset.component';
 import { Produit } from './views/produit/produit.module';
-import { UpdateUserComponent } from './views/superadmin/update-user/update-user.component';
 import { FournisseurModule } from './views/fournisseur/fournisseur.module';
-import { AddcategorieComponent } from './views/categorie/addcategorie/addcategorie.component';
-import { UpdatecategorieComponent } from './views/categorie/updatecategorie/updatecategorie.component';
-import { ListcategorieComponent } from './views/categorie/listcategorie/listcategorie.component';
 import { Categorie } from './views/categorie/categorie.module';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
@@ -84,7 +80,6 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
     SuperAdmin,
     FournisseurModule,
     Produit,
-
     Categorie,
     MatButtonToggleModule
 
