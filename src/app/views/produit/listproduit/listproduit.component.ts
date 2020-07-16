2441d4059652eb9ce20236ed1be07203009e60cc
achrafHaddad <achrafmail7@gmail.com>
2020-07-16 15:51:13 +0100

    list prod pour admin

--- src/app/views/produit/listproduit/listproduit.component.ts
+++ src/app/views/produit/listproduit/listproduit.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ProduitService } from '../../../service/produit.service';
-import * as jwt_decode from 'jwt-decode';
-import { AdminService } from '../../../service/admin.service';
-import { PageEvent } from '@angular/material/paginator';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ProduitService } from "../../../service/produit.service";
+import * as jwt_decode from "jwt-decode";
+import { AdminService } from "../../../service/admin.service";
+import { PageEvent } from "@angular/material/paginator";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-listproduit',
-  templateUrl: './listproduit.component.html',
-  styleUrls: ['./listproduit.component.css'],
+  selector: "app-listproduit",
+  templateUrl: "./listproduit.component.html",
+  styleUrls: ["./listproduit.component.css"],
 })
 export class ListproduitComponent implements OnInit {
   table;
@@ -18,36 +18,70 @@ export class ListproduitComponent implements OnInit {
   pageSizeOptions = [2, 5, 10];
   totalProd;
   currentPage = 1;
-  Search : "";
+  Search: "";
+  pme;
+  pmeTable;
   constructor(
     private produit: ProduitService,
     private adminservice: AdminService,
     private toastr: ToastrService,
-    private router: Router,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.getproduit();
+    this.getproduit(this.pme);
+    this.getPmeByAdmin();
+  }
+  getPmeByAdmin() {
+    this.adminservice
+      .getPmeByAdminId(this.decoded.data._id, this.pageSize, this.currentPage)
+      .subscribe((res: { pme; count }) => {
+        this.pmeTable = res.pme;
+      });
   }
 
   onChange(pageData: PageEvent) {
     this.currentPage = pageData.pageIndex + 1;
     this.pageSize = pageData.pageSize;
-    this.produit
-      .Getallproduit(this.decoded.data.pme, this.pageSize, this.currentPage)
-      .subscribe((res: { stocks; count }) => {
-        this.table = res.stocks;
-        this.totalProd = res.count;
-      });
+    if (this.decoded.data.role === "admin") {
+      this.produit
+        .Getallproduit(this.pme, this.pageSize, this.currentPage)
+        .subscribe((res: { stocks; count }) => {
+          this.table = res.stocks;
+          this.totalProd = res.count;
+        });
+    } else {
+      this.produit
+        .Getallproduit(this.decoded.data.pme, this.pageSize, this.currentPage)
+        .subscribe((res: { stocks; count }) => {
+          this.table = res.stocks;
+          this.totalProd = res.count;
+        });
+    }
+  }
+
+  onChangePme(event) {
+    this.pme = event.target.value;
+
+    this.getproduit(this.pme);
   }
   /*********************get produit ************ */
-  getproduit() {
-    this.produit
-      .Getallproduit(this.decoded.data.pme, this.pageSize, this.currentPage)
-      .subscribe((res: { stocks; count }) => {
-        this.table = res.stocks;
-        this.totalProd = res.count;
-      });
+  getproduit(pme) {
+    if (this.decoded.data.role === "admin") {
+      this.produit
+        .Getallproduit(pme, this.pageSize, this.currentPage)
+        .subscribe((res: { stocks; count }) => {
+          this.table = res.stocks;
+          this.totalProd = res.count;
+        });
+    } else {
+      this.produit
+        .Getallproduit(this.decoded.data.pme, this.pageSize, this.currentPage)
+        .subscribe((res: { stocks; count }) => {
+          this.table = res.stocks;
+          this.totalProd = res.count;
+        });
+    }
   }
   /*******************delete produit ******************* */
   delete(i, id) {
@@ -55,8 +89,8 @@ export class ListproduitComponent implements OnInit {
       .DeleteProduitById(this.decoded.data.pme, id)
       .subscribe((res: any) => {
         this.table.splice(i, 1);
-        this.getproduit();
-        return this.toastr.success('Produit Deleted with succesfully');
+        this.getproduit(this.pme);
+        return this.toastr.success("Produit Deleted with succesfully");
       });
   }
 }
--- src/app/views/superadmin/superadmin-rouring.module.ts
+++ src/app/views/superadmin/superadmin-rouring.module.ts
@@ -1,74 +1,68 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ListsocieterComponent } from './listsocieter/listsocieter.component';
-import { AddsocieterComponent } from './addsocieter/addsocieter.component';
-import { UpdatesocieterComponent } from './updatesocieter/updatesocieter.component';
-import { AdduserComponent } from './adduser/adduser.component';
-import { ListUSERComponent } from './list-user/list-user.component';
-import { UpdateUserComponent } from './update-user/update-user.component';
-
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ListsocieterComponent } from "./listsocieter/listsocieter.component";
+import { AddsocieterComponent } from "./addsocieter/addsocieter.component";
+import { UpdatesocieterComponent } from "./updatesocieter/updatesocieter.component";
+import { AdduserComponent } from "./adduser/adduser.component";
+import { ListUSERComponent } from "./list-user/list-user.component";
+import { UpdateUserComponent } from "./update-user/update-user.component";
 
 const routes: Routes = [
-    {
-        path: '',
-        data: {
-            title: 'Administration'
-        },
-        children: [
-            {
-                path: 'listsociete',
-                component: ListsocieterComponent,
-                data: {
-                    title: 'List Des Societé'
-                }
-            },
-            {
-                path: 'addsociete',
-                component: AddsocieterComponent,
-                data: {
-                    title: 'Add Societé'
-                }
-            },
-            {
-                path: 'updatesociete/:id',
-                component: UpdatesocieterComponent,
-                data: {
-                    title: 'Update Societé'
-                }
-            },
-            {
-                path : 'adduser',
-                component: AdduserComponent,
-                data: {
-                    title: 'Add User'
-                }
-
-            },
-            {
-                path : 'listuser',
-                component: ListUSERComponent,
-                data: {
-                    title: 'List User'
-                }
-
-            },
-            {
-                path : 'updateuser/:id',
-                component: UpdateUserComponent,
-                data: {
-                    title: 'Update User'
-                }
-
-            }
-        ],
+  {
+    path: "",
+    data: {
+      title: "Administration",
     },
-
+    children: [
+      {
+        path: "listsociete",
+        component: ListsocieterComponent,
+        data: {
+          title: "List Des Societé",
+        },
+      },
+      {
+        path: "addsociete",
+        component: AddsocieterComponent,
+        data: {
+          title: "Add Societé",
+        },
+      },
+      {
+        path: "updatesociete/:id",
+        component: UpdatesocieterComponent,
+        data: {
+          title: "Update Societé",
+        },
+      },
+      {
+        path: "adduser",
+        component: AdduserComponent,
+        data: {
+          title: "Add User",
+        },
+      },
+      {
+        path: "listuser",
+        component: ListUSERComponent,
+        data: {
+          title: "List User",
+        },
+      },
+      {
+        path: "updateuser/:id",
+        component: UpdateUserComponent,
+        data: {
+          title: "Update User",
+        },
+      },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
-    //   declarations: [ListsocieterComponent]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+  //   declarations: [ListsocieterComponent]
 })
-export class SuperAdminRouting { }
+export class SuperAdminRouting {}
