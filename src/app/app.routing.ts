310eb73c9610d3a2c28570485e47af45d50fb797
achrafHaddad <achrafmail7@gmail.com>
2020-07-20 15:18:09 +0100

    fix lazy loading

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,116 +1,116 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 // Import Containers
-import { DefaultLayoutComponent } from './containers';
+import { DefaultLayoutComponent } from "./containers";
 
-import { P404Component } from './views/error/404.component';
-import { P500Component } from './views/error/500.component';
-import { LoginComponent } from './common/login/login.component';
-import { RegisterComponent } from './common/register/register.component';
-import { ForgetComponent } from './common/forget/forget.component';
-import { ResetComponent } from './common/reset/reset.component';
-import { GuardAuthorizGuard } from './guards/guard-authoriz.guard';
+import { P404Component } from "./views/error/404.component";
+import { P500Component } from "./views/error/500.component";
+import { LoginComponent } from "./common/login/login.component";
+import { RegisterComponent } from "./common/register/register.component";
+import { ForgetComponent } from "./common/forget/forget.component";
+import { ResetComponent } from "./common/reset/reset.component";
+import { GuardAuthorizGuard } from "./guards/guard-authoriz.guard";
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
   },
   {
-    path: 'reset-password',
+    path: "reset-password",
     component: ResetComponent,
     data: {
-      title: 'Page reset',
+      title: "Page reset",
     },
   },
   {
-    path: 'forget',
+    path: "forget",
     component: ForgetComponent,
     data: {
-      title: 'Page forget',
+      title: "Page forget",
     },
   },
   {
-    path: 'register',
+    path: "register",
     component: RegisterComponent,
     data: {
-      title: 'Page register',
+      title: "Page register",
     },
   },
   {
-    path: '404',
+    path: "404",
     component: P404Component,
     data: {
-      title: 'Page 404',
+      title: "Page 404",
     },
   },
   {
-    path: '500',
+    path: "500",
     component: P500Component,
     data: {
-      title: 'Page 500',
+      title: "Page 500",
     },
   },
   {
-    path: 'login',
+    path: "login",
     component: LoginComponent,
     data: {
-      title: 'Login Page',
+      title: "Login Page",
     },
   },
   {
-    path: 'home',
+    path: "home",
     component: DefaultLayoutComponent,
     canActivate: [GuardAuthorizGuard],
     data: {
-      title: 'Home',
+      title: "Home",
     },
     children: [
       {
-        path: 'dashboard',
+        path: "dashboard",
         loadChildren: () =>
-          import('./views/dashboard/dashboard.module').then(
+          import("./views/dashboard/dashboard.module").then(
             (m) => m.DashboardModule
           ),
       },
       {
-        path: 'superadmin',
+        path: "superadmin",
         loadChildren: () =>
-          import('./views/superadmin/superadmin.module').then(
+          import("./views/superadmin/superadmin.module").then(
             (m) => m.SuperAdmin
           ),
       },
       {
-        path: 'produit',
+        path: "produit",
         loadChildren: () =>
-          import('./views/produit/produit.module').then((m) => m.Produit),
+          import("./views/produit/produit.module").then((m) => m.Produit),
       },
       {
-        path: 'fournisseur',
+        path: "fournisseur",
         loadChildren: () =>
-          import('./views/fournisseur/fournisseur.module').then(
+          import("./views/fournisseur/fournisseur.module").then(
             (m) => m.FournisseurModule
           ),
         canActivate: [GuardAuthorizGuard],
       },
       {
-        path: 'categorie',
+        path: "categorie",
         loadChildren: () =>
-          import('./views/categorie/categorie.module').then((m) => m.Categorie),
+          import("./views/categorie/categorie.module").then((m) => m.Categorie),
       },
 
       {
-        path: 'setting',
+        path: "setting",
         loadChildren: () =>
-          import('./views/settings/settings.module').then(
+          import("./views/settings/settings.module").then(
             (m) => m.SettingsModule
           ),
       },
     ],
   },
-  { path: '**', component: P404Component },
+  { path: "**", component: P404Component },
 ];
 
 @NgModule({
