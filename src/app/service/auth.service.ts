617c748621c0dda2125bcb9572b75c993fd82275
Adem Baroudi <adembaroudi3177@gmail.com>
2020-07-21 10:35:38 +0100

    adem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -44,7 +44,6 @@ export class AuthService {
   // connection sur la base de donnée et register societé
   Register(data) {
     const url = `${this.BaseUrl}/admin/register`;
-    this.router.navigateByUrl("/login");
     return this.http.post(url, data);
   }
   // connection sur la base de donnée et login societé
